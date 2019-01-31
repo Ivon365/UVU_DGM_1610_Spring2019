ccae9ba58f0b5445b86967b15f603dffe0ae3389
Texus Noe <texus.noe@gmail.com>
2019-01-30 17:08:55 -0700

    Added function and conditional statements.

---
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	public string weather;
+
+
+	void Start(){
+		Weather(weather);
+	}
+
+	void Weather(string weatherState){
+
+		if(weatherState == "Sunny"){
+			print("The sun shining today!");
+		}
+		else if(weatherState == "Raining"){
+			print("It is soggy and wet today!");
+		}
+		else if(weatherState == "Windy"){
+			print("It is blowing harder than a desk fan today!");
+		}
+		else if(weatherState == "Snowing"){
+			print("Baby it's cold outside!");
+		}
+		else if(weatherState == "Foggy"){
+			print("Visiblity is very poor due to fog!");
+		}
+		else{
+			print("I don't understand "+weatherState);
+		}
+	}
+}
