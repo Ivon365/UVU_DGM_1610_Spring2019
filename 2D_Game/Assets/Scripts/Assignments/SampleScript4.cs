0861c644c946c9901af266ce38e63dc22dd221d7
Texus Noe <texus.noe@gmail.com>
2019-02-04 21:14:36 -0700

    Created 4th sample script

---
+++ 2D_Game/Assets/Scripts/Assignments/SampleScript4.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript4 : MonoBehaviour {
+
+	public string lightState;
+
+//Use this for initialization
+	void Start(){
+		Light(lightState);
+	}
+
+	void Light(string lightState){
+
+		if(lightState == "On"){
+			print("You turn on the light.");
+		}
+		else if(lightState == "Off"){
+			print("You turn off the light.");
+		}
+		else{
+			print("I don't understand "+lightState);
+		}
+	}
+}
