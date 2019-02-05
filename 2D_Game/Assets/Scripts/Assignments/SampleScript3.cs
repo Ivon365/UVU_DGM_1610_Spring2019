f8b572e60549c8675643cf042134f8e9e0f4e5df
Texus Noe <texus.noe@gmail.com>
2019-02-04 20:59:45 -0700

    Created 3rd sample script

---
+++ 2D_Game/Assets/Scripts/Assignments/SampleScript3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript3 : MonoBehaviour {
+
+	public string walletState;
+
+
+	void Start(){
+		Wallet(walletState);
+	}
+
+	void Wallet(string walletState){
+
+		if(walletState == "Open"){
+			print("You opened your wallet.");
+		}
+		else if(walletState == "Close"){
+			print("You closed your wallet.");
+		}
+		else{
+			print("I don't understand "+walletState);
+		}
+	}
+}
