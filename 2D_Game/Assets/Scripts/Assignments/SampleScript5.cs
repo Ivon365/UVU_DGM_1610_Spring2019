8782645b3ef8db787c5c1c25c2b74fcfaa5bf268
Texus Noe <texus.noe@gmail.com>
2019-02-04 21:20:39 -0700

    Created 5th sample script

---
+++ 2D_Game/Assets/Scripts/Assignments/SampleScript5.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript5 : MonoBehaviour {
+
+	public string gameState;
+
+
+	void Start(){
+		Game(gameState);
+	}
+
+	void Game(string gameState){
+
+		if(gameState == "Link"){
+			print("HIYA!");
+		}
+		else if(gameState == "Navi"){
+			print("Hey! Listen!");
+		}
+		else if(gameState == "Mario"){
+			print("It's a me! Mario!");
+		}
+		else if(gameState == "Luigi"){
+			print("I'm a back!");
+		}
+		else if(gameState == "Hayner"){
+			print("Our ______ are gone! Our ______ are gone! Stolen!");
+		}
+		else{
+			print("I don't understand "+gameState);
+		}
+	}
+}
