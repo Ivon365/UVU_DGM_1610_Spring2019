4a928939032ee544fde5e83993eea19102c53327
Texus Noe <texus.noe@gmail.com>
2019-02-11 17:19:55 -0700

    Learned about inheritance and collisions

---
+++ 2D_Game/Assets/Scripts/Assignments/Animal.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//inheritance from MonoBehaviour
+public class Animal : MonoBehaviour
+{
+    public int health;
+    public int legs;
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/Tiger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//inheritence from Feline
+public class Tiger : Feline
+{
+    public bool banded;
+    // Start is called before the first frame update
+    void Start()
+    {
+       print("Roar");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
