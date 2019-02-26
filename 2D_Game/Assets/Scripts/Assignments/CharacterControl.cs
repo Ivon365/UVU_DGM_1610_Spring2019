4031e2885c2c4d7592821278b620be992e3def69
Texus <texus.noe@gmail.com>
2019-02-25 17:13:28 -0700

    Learned if statements and nested if conditional statements.

--- 2D_Game/Assets/Scripts/Assignments/CharacterControl.cs
+++ 2D_Game/Assets/Scripts/Assignments/CharacterControl.cs
@@ -15,24 +15,24 @@ private bool grounded;
 // this || that (this or that must be true)
 // !Not this (Think of not gates in minecraft if true won't run, if not true will run)
 
-	// Use this for initialization
-	void Start () {
-		grounded = true
+// Use this for initialization
+void Start () {
+	grounded = true
 
-	}
+}
 	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		} 
-		else if(Input.GetKey(KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
+// Update is called once per frame
+void Update () {
+	if(Input.GetKey(KeyCode.D)){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	} 
+	else if(Input.GetKey(KeyCode.A)){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
 	}
 		
-		if(Input.GetKeyDown(KeyCode.W)&& grounded){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
-		}
+	if(Input.GetKeyDown(KeyCode.W)&& grounded){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+	}
 	
 }
 }
---
+++ 2D_Game/Assets/Scripts/Assignments/IF_Statements.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IF_Statements : MonoBehaviour
+{
+    public string stopLight;
+    public bool isUtahn = false;
+
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(stopLight == "Red"){
+        //Block of code that is run if the condition is met
+            if(isUtahn){
+                print("STOP! The light is RED!")
+            }
+            else{
+                print("Thank you for stopping at the red light!")
+            }
+        }
+        else if (stopLight == "Yellow"){
+            if(isUtahn){
+                print("You got this brah!")
+            }
+            else{
+                print("Thank you for preparing to stop!")
+            }
+        }
+        else if (stopLight == "Green" && !isUtahn){
+            print("Check phone, turn up radio, sit at the light until almost yellow, then go!");
+        }
+        //if condition is NOT met then condition will run else code
+        else{
+            print("Sorry, you are driving in Utah!");
+        }
+    }
+}
