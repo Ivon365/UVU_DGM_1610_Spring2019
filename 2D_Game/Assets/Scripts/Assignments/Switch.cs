7b35dc48b7afec1771fc366df7066c3754175cd9
Texus <texus.noe@gmail.com>
2019-02-27 17:05:32 -0700

    Auto stash before merge of "master" and "origin/master"
    Learned switches.

---
+++ 2D_Game/Assets/Scripts/Assignments/Switch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public string suspect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MurderMystery(suspect);
+    }
+
+    void MurderMystery (string person)
+    {
+        
+    switch(person){
+        case "Mr. Ketchup":
+        case "Mr. Radish":
+            print("I was in the billiard room smoking a cigar and playing pool.");
+        break;
+        case "Mrs. Frysauce":
+            print("I was talking on the rotary phone with my mother Mrs. Mayo.");
+        break;
+        case "Mrs. Mayo":
+            print("I was in the kitchen cleaning up the dishes with the scrubber.");
+        break;
+        default:
+            print("I am not familiar with "+suspect+"!");
+        break;
+        }
+    }
+}
