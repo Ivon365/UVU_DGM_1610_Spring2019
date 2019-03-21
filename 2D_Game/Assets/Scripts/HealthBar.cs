286b80ed93cfff96a7a28ba8d6a91e69bff1182e
Texus Noe <texus.noe@gmail.com>
2019-03-20 18:01:59 -0600

    Added buggy health system

---
+++ 2D_Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	public int HealthWindow = 100;
+	public int currentHP;
+
+	public Text Health;
+
+	void Start () {
+		currentHP = HealthWindow;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Health.text = currentHP.ToString();
+		
+	}
+
+	public void DamageDealt(int amt){
+		currentHP -= amt;
+		if(currentHP <= 0){
+			currentHP = 0;
+			print("You died! Game over!");
+		}
+	}
+}
