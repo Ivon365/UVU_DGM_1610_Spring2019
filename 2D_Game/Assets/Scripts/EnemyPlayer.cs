2c64b6cf03a3299da4b1729aa9ab2941fdb09dfe
Texus Noe <texus.noe@gmail.com>
2019-03-24 12:59:16 -0600

    Fixed Health script and WIP on enemy script

---
+++ 2D_Game/Assets/Scripts/EnemyPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayer : MonoBehaviour {
+
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		damage = 5;
+		damage = GetComponent<amt>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 2D_Game/Assets/Scripts/HealthBar.cs
+++ 2D_Game/Assets/Scripts/HealthBar.cs
@@ -5,23 +5,31 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
 
-	public int HealthWindow = 100;
+	public int HealthWindow;
 	public int currentHP;
 
 	public Text Health;
 
 	void Start () {
+		//Sets HP to a value of 100 at the start of the game
+		HealthWindow = 100;
+		//Makes the Current HP 100 at the start
 		currentHP = HealthWindow;
+		//Calls on the text UI in Unity
+		Health = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Health.text = currentHP.ToString();
+		//Concats the " " plus the value of the current HP.
+		Health.text = " " + currentHP;
 		
 	}
 
 	public void DamageDealt(int amt){
+		//deducts HP depending on the amt of damage taken
 		currentHP -= amt;
+		//When HP goes to 0 then it will be game over.
 		if(currentHP <= 0){
 			currentHP = 0;
 			print("You died! Game over!");
