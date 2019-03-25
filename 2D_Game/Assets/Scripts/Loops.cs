ce654081604e4c1bde46e400545d96faa9550a0b
Texus <texus.noe@gmail.com>
2019-03-25 16:43:23 -0600

    Added a loop and for each loop

---
+++ 2D_Game/Assets/Scripts/Loops.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int bottles = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //For loop = operate within finite parameters
+        for(int i=100; i <= bottles; i--){
+            print(i +" bottles of milk on the wall.");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
