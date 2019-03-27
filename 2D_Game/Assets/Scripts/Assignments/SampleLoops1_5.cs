4ba3ad6075e7054f9026ec5b90810aec6e66d39c
Texus <texus.noe@gmail.com>
2019-03-27 17:56:07 -0600

    Added to sample loops

--- 2D_Game/Assets/Scripts/Assignments/SampleLoops1_5.cs
+++ 2D_Game/Assets/Scripts/Assignments/SampleLoops1_5.cs
@@ -12,46 +12,131 @@ public class SampleLoops1_5 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		for(int e=10; e <= dogs; e--){
-			print(e + " puppies are left sleeping.");
-		}
-
-		// while(dogs <= 50){
-		// 	print(dogs + " dogs are running around.");
+		// for(int e=10; e <= dogs; e--){
+		// 	print(e + " puppies are left sleeping.");
 		// }
 
-		for(int y=0; y >= yams; y++){
-			print(y + " yams are left in the can,");
-		}
+		// // while(dogs <= 50){
+		// // 	print(dogs + " dogs are running around.");
+		// // }
 
-		// while(yams >= 0){
-		// 	print(yams + " yams are gross.");
+		// for(int y=0; y >= yams; y++){
+		// 	print(y + " yams are left in the can,");
 		// }
 
-		for(int a=1; a >= shirts; a++){
-			print(a + " shirts on my body");
-		}
+		// // while(yams >= 0){
+		// // 	print(yams + " yams are gross.");
+		// // }
 
-		// while(shirts >= 1){
-		// 	print(shirts + " shirts are awesome");
+		// for(int a=1; a >= shirts; a++){
+		// 	print(a + " shirts on my body");
 		// }
 
-		for(int t=0; t >= straws; t++){
-			print(t + " straws are in my cup.");
-		}
+		// // while(shirts >= 1){
+		// // 	print(shirts + " shirts are awesome");
+		// // }
 
-		// while(straws >= 0){
-		// 	print(straws + " straws puncturing the lid");
+		// for(int t=0; t >= straws; t++){
+		// 	print(t + " straws are in my cup.");
 		// }
 
-		for(int p=0; p >= plates; p++){
-			print(p + " plates purchased today");
-		}
+		// // while(straws >= 0){
+		// // 	print(straws + " straws puncturing the lid");
+		// // }
+
+		// for(int p=0; p >= plates; p++){
+		// 	print(p + " plates purchased today");
+		// }
 
 		// while(plates >= 0){
 		// 	print(plates + " plates are in the cupboard");
 		// }
 		
+		// bool doLikeGames = false;
+		// bool isBoss = false;
+		// bool isCool = false;
+		// bool isDangerous = false;
+		// bool isAmazingAtGolf = false;
+
+		// do
+		// {
+		// 	print("You are not good at games.");
+		// } while (doLikeGames == true);
+
+		// do
+		// {
+		// 	print("You are not the boss of me!");
+		// } while (isBoss == true);
+
+		// do
+		// {
+		// 	print("You are not cool, man.");
+		// } while (isCool == true);
+
+		// do
+		// {
+		// 	print("You aren't a dangerous person.");
+		// } while (isDangerous == true);
+
+		// do
+		// {
+		// 	print("You are not good at golf.");
+		// } while (isAmazingAtGolf == true);
+
+		// string[] straws = new string[3];
+
+		// straws[0] = "First straw";
+		// straws[1] = "Second straw";
+		// straws[2] = "Third straw";
+
+		// foreach(string cups in straws)
+		// {
+		// 	print(cups);
+		// }
+
+		// string[] lost = new string[3];
+
+		// lost[0] = "You're lost";
+		// lost[1] = "at sea";
+		// lost[2] = "and can't be found.";
+
+		// foreach(string island in lost)
+		// {
+		// 	print(island);
+		// }
+
+		// string[] princesses = new string[3];
+
+		// princesses[0] = "Cinderella";
+		// princesses[1] = "Snow White";
+		// princesses[2] = "Belle";
+
+		// foreach(string shoes in princesses)
+		// {
+		// 	print(shoes);
+		// }
+
+		// string[] phones = new string[3];
+
+		// phones[0] = "Sasmoso";
+		// phones[1] = "Toogle";
+		// phones[2] = "RG";
+
+		// foreach(string calls in phones)
+		// {
+		// 	print(calls);
+		// }
+
+		// string[] rejects = new string[3];
+
+		// rejects[0] = "First reject";
+		// rejects[1] = "Second reject";
+		// rejects[2] = "Third reject";
+
+		// foreach(string crap in rejects)
+		// {
+		// 	print(crap);
+		// }
 	}
 	
 	// Update is called once per frame
--- 2D_Game/Assets/Scripts/Loops.cs
+++ 2D_Game/Assets/Scripts/Loops.cs
@@ -6,25 +6,25 @@ public class Loops : MonoBehaviour
 {
 
 
-    // public int bottles = 0;
+    public int bottles = 0;
     // Start is called before the first frame update
     void Start()
     {
-        //For loop = operate within finite parameters
-        // for(int i=100; i <= bottles; i--){
-        //     print(i +" bottles of milk on the wall.");
-        // }
+        // For loop = operate within finite parameters
+        for(int i=100; i <= bottles; i--){
+            print(i +" bottles of milk on the wall.");
+        }
         
-        // bool shouldContinue = false;
+        bool shouldContinue = false;
 
-        // //do-while loops always run a minimum of once even if the statement is false.
-        // do
-        // {
-        //     print ("Hello World");
-        // }
-        // while(shouldContinue == true);
+        //do-while loops always run a minimum of once even if the statement is false.
+        do
+        {
+            print ("Hello World");
+        }
+        while(shouldContinue == true);
 
-        //foreach loops are great for looping through an array once per array.
+        // foreach loops are great for looping through an array once per array.
 
         string[] strings = new string[3];
 
@@ -37,9 +37,11 @@ public class Loops : MonoBehaviour
             print(item);
         }
 
-        // while(bottles <= 100){
-        //     print(bottles + " bottles of milk on the wall");
-        // }
+        //while loops loop over and over. No limit to while loops.
+        
+        while(bottles <= 100){
+            print(bottles + " bottles of milk on the wall");
+        }
         
     }
 
