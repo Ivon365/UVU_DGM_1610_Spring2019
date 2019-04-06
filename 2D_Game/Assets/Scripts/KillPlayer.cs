94e4977b1ab158b1d192ff118be017ddcc67b86f
Texus Noe <texus.noe@gmail.com>
2019-04-06 11:09:40 -0600

    Replaced Kill Player script

--- 2D_Game/Assets/Scripts/KillPlayer.cs
+++ 2D_Game/Assets/Scripts/KillPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillPlayer : Monobehaviour {
+public class KillPlayer : MonoBehaviour {
 
 
     public LevelManager levelManager;
