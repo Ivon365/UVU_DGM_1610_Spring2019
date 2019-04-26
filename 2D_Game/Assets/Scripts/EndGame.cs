a678d28d6008d6fe58a9061e039f72c44a4e1965
Texus Noe <texus.noe@gmail.com>
2019-04-25 23:55:54 -0600

    Read full description below
    Added background art, Added end sprite, Added game over scene with exit button, Added more ground, enemies, coins, and walls, Added Wall Art, Added game over art.

---
+++ 2D_Game/Assets/Scripts/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public int levelToLoad;
+void OnTriggerEnter2D (Collider2D other){
+    if (other.tag == "Player"){
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    }
+
+public void LevelExit(){
+    Application.Quit();
+    }
+}
---
+++ 2D_Game/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+public void LevelExit(){
+    Debug.Log("Player has quit game.");
+        Application.Quit();
+    }
+}
